ead7b600c79cf662b1a7b9816610726926022e5c
shaneworld <516097815@qq.com>
2024-01-12 18:50:18 +0800

    A big change

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,10 @@ if status is-interactive
   alias ls "exa --icons -l"
   alias ll "exa --icons -al -T -L=2"
   alias pc "sudo pacman -Sc"
+  set PATH ~/.emacs.d/bin/ $PATH
+  set PATH /home/shane/Development/rustlings/target/release $PATH
   set -x https_proxy http://127.0.0.1:7890
   set -x http_proxy http://127.0.0.1:7890
   set -x all_proxy socks5://127.0.0.1:7890
 end
+fish_add_path /home/shane/.spicetify
