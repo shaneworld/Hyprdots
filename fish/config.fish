a009cee42106d51fa1503830f1dfb32a7cfdfc8b
shaneworld <516097815@qq.com>
2024-01-23 21:43:23 +0800

    Update some details

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,12 @@
 if status is-interactive
   set -g fish_greeting
   colorscript -e elfman
-	alias vim "nvim"
+  alias vim "nvim"
   alias ls "exa --icons -l"
   alias ll "exa --icons -al -T -L=2"
   alias pc "sudo pacman -Sc"
   set PATH /home/shane/Development/rustlings/target/release $PATH
+  set PATH /home/shane/.cargo/bin/ $PATH
   # set -x https_proxy http://127.0.0.1:7890
   # set -x http_proxy http://127.0.0.1:7890
   # set -x all_proxy socks5://127.0.0.1:7890
