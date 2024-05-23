2bfb3935a32bb113ab41c929e66025a67d8ca434
shaneworld <s516097815@gmail.com>
2024-05-23 15:08:03 +0800

    Add doom emacs basic config

--- fish/config.fish
+++ fish/config.fish
@@ -3,11 +3,11 @@ if status is-interactive
   nitch
   set -g fish_greeting
   alias vim "nvim"
+  alias y "yazi"
   alias ls "exa --icons -l"
   alias ll "exa --icons -al"
   alias pc "sudo pacman -Sc"
   alias se "sudoedit"
-  alias code "codium --unity-launch --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime"
   # alias cat "bat"
   set -x https_proxy http://localhost:7897
   set -x http_proxy http://localhost:7897
